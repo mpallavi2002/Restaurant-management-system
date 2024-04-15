ed885db071695acdfc2c57fc04d532f3e250c7a9
kevallad02 <pmishr15@my.centennialcollege.ca>
2024-04-15 19:07:19 +0530

    final push

--- MERN Restaurant management system 1/frontend/src/App.js
+++ MERN Restaurant management system 1/frontend/src/App.js
@@ -1,39 +1,73 @@
-import "./App.css";
-import React from 'react';
-import { Routes, Route, BrowserRouter as Router } from "react-router-dom";
-import Navbar from "./components/Navbar";
-import Login from "./components/Login";
-import Register from "./components/Register";
-import ForgotPassword from "./components/ForgotPassword";
-import PrivateComponent from "./components/PrivateComponent";
-import Recipes from "./components/Recipes";
-import AddRecipe from "./components/AddRecipe";
-import LikedProducts from "./components/likedProducts";
-import TeamComponent from "./components/team.component";
-import HomeComponent from "./components/home.component";
-import Landing from "./components/landing.component";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import BillsPage from "./pages/BillsPage";
+import CartPage from "./pages/CartPage";
+import CustomerPage from "./pages/CustomerPage";
+import Homepage from "./pages/Homepage";
+import ItemPage from "./pages/itemPage";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 function App() {
   return (
-    <Router>
-      <Navbar />
-
-      <Routes>
-        <Route path="/" element={<Landing />} /> 
-        <Route path="home" element={<HomeComponent />} /> 
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Register />} />
-        <Route path="/forgotPassword" element={<ForgotPassword />} />
-
-        <Route element={<PrivateComponent />}>
-          <Route path="/favouriteRecipes" element={<LikedProducts />} />
-          <Route path="/recipes" element={<Recipes />} />
-          <Route path="/addRecipe" element={<AddRecipe />} />
-          <Route path="/team" element={<TeamComponent />} /> 
-        </Route>
-      </Routes>
-    </Router>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <ProtectedRoute>
+                <Homepage />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/items"
+            element={
+              <ProtectedRoute>
+                <ItemPage />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/cart"
+            element={
+              <ProtectedRoute>
+                <CartPage />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/bills"
+            element={
+              <ProtectedRoute>
+                <BillsPage />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/customers"
+            element={
+              <ProtectedRoute>
+                <CustomerPage />
+              </ProtectedRoute>
+            }
+          />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
-export default App;
+
+
+export function ProtectedRoute({ children }) {
+  if (localStorage.getItem("user")) {
+    return children;
+  } else {
+    return <Navigate to="/login" />;
+  }
+}
+
+export default App
